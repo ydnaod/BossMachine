06e546669f5642d98e44eb9bd19d5d44073f850b
ydnaod <65431123+ydnaod@users.noreply.github.com>
2020-12-12 11:51:26 -0500

    fix the weird auto-import that broke the whole app

--- server/minions.js
+++ server/minions.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { default: minions } = require('../browser/store/minions');
+//const { default: minions } = require('../browser/store/minions');
 const minionsRouter = express.Router();
 const db = require('./db')
 
-/*minionsRouter.param('minionId', (req, res, next, id) => {
+minionsRouter.param('minionId', (req, res, next, id) => {
     const minions = db.getAllFromDatabase('minions');
     const minionIndex = minions.findIndex(minion => minion.id === id);
     if(minionIndex !== -1){
@@ -14,7 +14,7 @@ const db = require('./db')
         res.status(404).send('oops not found');
     }
     console.log('here')
-})*/
+})
 
 minionsRouter.get('/', (req, res, next) => {
     res.send(db.getAllFromDatabase('minions'));
@@ -26,9 +26,9 @@ minionsRouter.post('/', (req, res, next) => {
     res.send(newMinion);
 })
 
-/*minionsRouter.get('/:minionId', (req, res, next) => {
+minionsRouter.get('/:minionId', (req, res, next) => {
     const minionList = db.getAllFromDatabase('minions');
     res.send(minionList[req.minionIndex]);
-})*/
+})
 
 module.exports = minionsRouter;
