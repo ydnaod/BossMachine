3ad6944b4558445d2d28fe84899ebd5fe39eb562
ydnaod <65431123+ydnaod@users.noreply.github.com>
2020-12-12 11:47:53 -0500

    broke some stuff

--- server/minions.js
+++ server/minions.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const { default: minions } = require('../browser/store/minions');
 const minionsRouter = express.Router();
 const db = require('./db')
 
+/*minionsRouter.param('minionId', (req, res, next, id) => {
+    const minions = db.getAllFromDatabase('minions');
+    const minionIndex = minions.findIndex(minion => minion.id === id);
+    if(minionIndex !== -1){
+        req.minionIndex = minionIndex;
+        next();
+    }
+    else{
+        res.status(404).send('oops not found');
+    }
+    console.log('here')
+})*/
 
 minionsRouter.get('/', (req, res, next) => {
     res.send(db.getAllFromDatabase('minions'));
@@ -12,5 +25,10 @@ minionsRouter.post('/', (req, res, next) => {
     db.addToDatabase('minions', newMinion);
     res.send(newMinion);
 })
+
+/*minionsRouter.get('/:minionId', (req, res, next) => {
+    const minionList = db.getAllFromDatabase('minions');
+    res.send(minionList[req.minionIndex]);
+})*/
 
 module.exports = minionsRouter;
